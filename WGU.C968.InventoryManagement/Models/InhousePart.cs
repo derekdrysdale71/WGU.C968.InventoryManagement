04536f10a767b70a04e3088b6d7d4165e1a227f3
Derek Drysdale <derek.drysdale@avistacorp.com>
2020-02-04 11:26:35 -0800

    add data models and mvvc structure

---
+++ WGU.C968.InventoryManagement/Models/InhousePart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WGU.C968.InventoryManagement.Models
+{
+    public class InhousePart : Part
+    {
+        public int MachineId { get; set; }
+    }
+}
---
+++ WGU.C968.InventoryManagement/Models/Part.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WGU.C968.InventoryManagement.Models
+{
+    public abstract class Part
+    {
+        public int PartId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool InStock { get; set; }
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+    }
+}
