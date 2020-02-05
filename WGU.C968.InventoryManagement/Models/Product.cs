d993d2474f854ac7bb80a5a9338d963f41d77863
Derek Drysdale <Derek.Drysdale@avistacorp.com>
2020-02-04 17:38:39 -0800

    add product actions

--- WGU.C968.InventoryManagement/Models/Product.cs
+++ WGU.C968.InventoryManagement/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace WGU.C968.InventoryManagement.Models
@@ -20,5 +21,36 @@ namespace WGU.C968.InventoryManagement.Models
         public int Max { get; set; }
 
         public ObservableCollection<Part> AssociatedParts = new ObservableCollection<Part>();
+
+        public Product()
+        {
+
+        }
+
+
+        public void AddAssociatedPart(Part part)
+        {
+            AssociatedParts.Add(part);
+        }
+
+        public bool RemoveAssociatedPart(int partId)
+        {
+            var partToRemove = AssociatedParts.Where(p => p.PartId == partId).FirstOrDefault();
+            if (partToRemove == null)
+                throw new Exception(message: $"A part with ID #{partId} is not associated with this product.");
+
+            AssociatedParts.Remove(partToRemove);
+            return true;
+        }
+
+        public Part LookupAssociatedPart(int partId)
+        {
+            var part = AssociatedParts.Where(p => p.PartId == partId).FirstOrDefault();
+
+            if (part == null)
+                throw new Exception(message: $"A part with ID #{partId} is not associated with this product.");
+
+            return part;
+        }
     }
 }
