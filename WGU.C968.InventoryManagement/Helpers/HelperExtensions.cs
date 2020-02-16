9a18776cb9b9bc364b7da1ead769ea3af312c4ad
Derek Drysdale <Derek.Drysdale@avistacorp.com>
2020-02-16 11:31:43 -0800

    add helpers

---
+++ WGU.C968.InventoryManagement/Helpers/HelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WGU.C968.InventoryManagement.Helpers
+{
+    public static class HelperExtensions
+    {
+        public static int GetNextId(this List<int> ids)
+        {
+            return ids.Max() + 1;
+        }
+    }
+}
